fc6c083cc8872b92b618b11d33982cc7981dd65b
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-29 09:11:01 +0900

    add Project 16, part 1
    https://www.hackingwithswift.com/100/swiftui/79

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,9 +8,60 @@
 
 import SwiftUI
 
+//class User: ObservableObject {
+//    @Published var name = "Taylor Swift"
+//}
+//
+//struct EditView: View {
+//    @EnvironmentObject var user: User
+//
+//    var body: some View {
+//        TextField("Name", text: $user.name)
+//    }
+//}
+//
+//struct DisplayView: View {
+//    @EnvironmentObject var user: User
+//
+//    var body: some View {
+//        Text(user.name)
+//    }
+//}
+//
+//struct ContentView: View {
+//    let user = User()
+//
+//    var body: some View {
+//        VStack {
+//            EditView()
+//            DisplayView()
+//        }
+//        .environmentObject(user)
+//    }
+//}
+
 struct ContentView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = 1
+            }
+            .tabItem {
+                Image(systemName: "star")
+                Text("One")
+            }
+            .tag(0)
+            
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+            }
+            .tag(1)
+        }
     }
 }
 
