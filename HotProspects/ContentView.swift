b2d47ba7181e3d0f425809b418f3d6c18c150c5a
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-30 09:36:31 +0900

    add Project 16, part 2
    https://www.hackingwithswift.com/100/swiftui/80

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,60 +8,92 @@
 
 import SwiftUI
 
-//class User: ObservableObject {
-//    @Published var name = "Taylor Swift"
-//}
-//
-//struct EditView: View {
-//    @EnvironmentObject var user: User
-//
-//    var body: some View {
-//        TextField("Name", text: $user.name)
-//    }
-//}
-//
-//struct DisplayView: View {
-//    @EnvironmentObject var user: User
-//
-//    var body: some View {
-//        Text(user.name)
-//    }
+//enum NetworkError: Error {
+//    case badURL, requestFailed, unknown
 //}
 //
 //struct ContentView: View {
-//    let user = User()
+//    var body: some View {
+//        Text("Hello, World!")
+//        .onAppear {
+//            self.fetchData(from: "https://www.apple.com") { result in
+//                switch result {
+//                case .success(let str):
+//                    print(str)
+//                case .failure(let error):
+//                    switch error {
+//                    case .badURL:
+//                        print("Bad URL")
+//                    case .requestFailed:
+//                        print("Network problems")
+//                    case .unknown:
+//                        print("Unknown error")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+//        // check the URL is OK, otherwise return with a failure
+//        guard let url = URL(string: urlString) else {
+//            completion(.failure(.badURL))
+//            return
+//        }
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            // the task has completed – push our work back to the main thread
+//            DispatchQueue.main.async {
+//                if let data = data {
+//                    // success: convert the data to a string and send it back
+//                    let stringData = String(decoding: data, as: UTF8.self)
+//                    completion(.success(stringData))
+//                } else if error != nil {
+//                    // any sort of network failure
+//                    completion(.failure(.requestFailed))
+//                } else {
+//                    // this ought not to be possible, yet here we are
+//                    completion(.failure(.unknown))
+//                }
+//            }
+//        }.resume()
+//    }
+//}
+
+//struct ContentView: View {
+//    @ObservedObject var updater = DelayedUpdater()
 //
 //    var body: some View {
-//        VStack {
-//            EditView()
-//            DisplayView()
+//        Text("Value is: \(updater.value)")
+//    }
+//}
+//
+//class DelayedUpdater: ObservableObject {
+////    @Published var value = 0
+//
+//    var value = 0 {
+//        willSet {
+//            objectWillChange.send()
 //        }
-//        .environmentObject(user)
+//    }
+//
+//    init() {
+//        for i in 1...10 {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+//                self.value += 1
+//            }
+//        }
 //    }
 //}
 
 struct ContentView: View {
-    @State private var selectedTab = 0
-    
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    self.selectedTab = 1
-            }
-            .tabItem {
-                Image(systemName: "star")
-                Text("One")
-            }
-            .tag(0)
-            
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-            }
-            .tag(1)
-        }
+        Image("example")
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
