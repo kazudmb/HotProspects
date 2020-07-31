ac2ae25fc37de22f2f58cc0997f10c9dfeb03875
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-01 07:59:11 +0900

    add Project 16, part 4
    https://www.hackingwithswift.com/100/swiftui/82

---
+++ HotProspects/MeView.swift
@@ -0,0 +1,21 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by KazukiNakano on 2020/08/01.
+//  Copyright © 2020 dmb. All rights reserved.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
